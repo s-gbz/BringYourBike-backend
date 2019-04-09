45acb41136a99653f8d0a424a537ce8b2f25820b
Sebastian Trzpiot <mail@trzpiot.de>
2019-04-09 12:58:16 +0200

    Add OwnerService

---
+++ bring-your-bike-java/src/main/java/de/trzpiot/bringyourbike/service/OwnerService.java
@@ -0,0 +1,34 @@
+package de.trzpiot.bringyourbike.service;
+
+import de.trzpiot.bringyourbike.domain.Bike;
+import de.trzpiot.bringyourbike.repository.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OwnerService {
+    private final BikeRepository bikeRepository;
+
+    @Autowired
+    public OwnerService(BikeRepository bikeRepository) {
+        this.bikeRepository = bikeRepository;
+    }
+
+    public Long addBike(Bike bike) {
+        return bikeRepository.save(bike).getId();
+    }
+
+    public void updateBike(Bike bike) {
+        bikeRepository.save(bike);
+    }
+
+    public Bike getOneBike(Long id) {
+        return bikeRepository.getOne(id);
+    }
+    
+    public List<Bike> getAllBikes() {
+        return bikeRepository.findAll();
+    }
+}
