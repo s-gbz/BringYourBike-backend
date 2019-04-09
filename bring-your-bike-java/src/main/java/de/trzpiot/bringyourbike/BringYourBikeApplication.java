4d1bc7e3c677fb89c82278e9df63dc4fd4fbf1b5
Sebastian Trzpiot <mail@trzpiot.de>
2019-04-09 12:17:49 +0200

    Initialize application

---
+++ bring-your-bike-java/src/main/java/de/trzpiot/bringyourbike/BringYourBikeApplication.java
@@ -0,0 +1,13 @@
+package de.trzpiot.bringyourbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BringYourBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BringYourBikeApplication.class, args);
+	}
+
+}
