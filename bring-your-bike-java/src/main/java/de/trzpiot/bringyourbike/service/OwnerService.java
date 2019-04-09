dfe93cd7a0ab467e91cc91c16a670d5ba7c438d5
Sebastian Trzpiot <mail@trzpiot.de>
2019-04-09 16:37:29 +0200

    Add using the light

--- bring-your-bike-java/src/main/java/de/trzpiot/bringyourbike/service/OwnerService.java
+++ bring-your-bike-java/src/main/java/de/trzpiot/bringyourbike/service/OwnerService.java
@@ -4,32 +4,50 @@ import de.trzpiot.bringyourbike.domain.Bike;
 import de.trzpiot.bringyourbike.exception.BikeNotFoundException;
 import de.trzpiot.bringyourbike.repository.BikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class OwnerService {
+    private final LightService lightService;
     private final BikeRepository bikeRepository;
+    private final String BRIDGE_IP;
+    private final String BRIDGE_USERNAME;
+    private final long LIGHT_ID;
 
     @Autowired
-    public OwnerService(BikeRepository bikeRepository) {
+    public OwnerService(LightService lightService, BikeRepository bikeRepository, Environment environment) {
+        this.lightService = lightService;
         this.bikeRepository = bikeRepository;
+
+        BRIDGE_IP = environment.getProperty("bridge.ip");
+        BRIDGE_USERNAME = environment.getProperty("bridge.username");
+        LIGHT_ID = Long.parseLong(Objects.requireNonNull(environment.getProperty("bridge.light.id")));
     }
 
     public Bike addBike(Bike bike) {
+        lightService.switchOn(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID);
+        lightService.setBrightness(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255);
+        setColor(bike.getPriority());
         return bikeRepository.save(bike);
     }
 
     public void updateBike(Bike bike) {
         Optional<Bike> bikeInDatabase = bikeRepository.getBikeByPin(bike.getPin());
 
-        if (bikeInDatabase.isPresent())
+        if (bikeInDatabase.isPresent()) {
+            lightService.switchOn(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID);
+            lightService.setBrightness(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255);
+            setColor(bike.getPriority());
             bikeRepository.save(bike);
-        else
+        } else {
             throw new BikeNotFoundException(String.format("Bike with PIN '%s' not found", bike.getPin()));
+        }
     }
 
     public Bike getBikeByPin(Long pin) {
@@ -50,6 +68,18 @@ public class OwnerService {
             Long pin = ThreadLocalRandom.current().nextLong(1000, 10000);
             if (bikeRepository.countBikeByPin(pin) == 0)
                 return pin;
+        }
+    }
+
+    private void setColor(long priority) {
+        if (priority == 0) { // Green
+            lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 76, 175, 80);
+        } else if (priority > 0 && priority <= 2) { // Blue
+            lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 33, 150, 243);
+        } else if (priority > 2 && priority <= 5) { // Yellow
+            lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255, 235, 59);
+        } else if (priority >= 6) { // Red
+            lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 244, 67, 54);
         }
     }
 }
