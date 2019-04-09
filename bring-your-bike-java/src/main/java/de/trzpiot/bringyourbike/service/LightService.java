353e0b7e9b95d18b739e0782cd721782fabf3f02
Sebastian Trzpiot <mail@trzpiot.de>
2019-04-09 16:11:46 +0200

    Add LightService

---
+++ bring-your-bike-java/src/main/java/de/trzpiot/bringyourbike/service/LightService.java
@@ -0,0 +1,15 @@
+package de.trzpiot.bringyourbike.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LightService {
+    static {
+        System.loadLibrary("bulbbattle");
+    }
+
+    public native void switchOn(String bridgeIp, String bridgeUsername, long lightId);
+    public native void switchOff(String bridgeIp, String bridgeUsername, long lightId);
+    public native void setColor(String bridgeIp, String bridgeUsername, long lightId, int r, int g, int b);
+    public native void setBrightness(String bridgeIp, String bridgeUsername, long lightId, int brightness);
+}
