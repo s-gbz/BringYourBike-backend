e6bb84a8850c5b2da0bfe8dbc58c6c522d4956b7
Sebastian Trzpiot <mail@trzpiot.de>
2019-04-09 15:33:39 +0200

    Refactor

--- bring-your-bike-java/src/main/java/de/trzpiot/bringyourbike/BringYourBikeApplication.java
+++ bring-your-bike-java/src/main/java/de/trzpiot/bringyourbike/BringYourBikeApplication.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BringYourBikeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BringYourBikeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BringYourBikeApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
